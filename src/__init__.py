ad96d73e4fcdfab5904ec914c66134833d677a88
Massili Kezzoul <massy.kezzoul@gmail.com>
2020-02-19 23:44:17 +0100

    Add translate_rating_value, extract_tags and updating extract_date, get method

--- src/__init__.py
+++ src/__init__.py
@@ -21,10 +21,17 @@ def main():
     for line in sys.stdin:
         line.rstrip()  # enleve le dernier '\n' de la ligne
         webpage = fatabyyano.get(line)
+        tags = fatabyyano.extract_tags(webpage)
+        date = fatabyyano.extract_date(webpage)
         claim = fatabyyano.extract_claim(webpage)
-        value = fatabyyano.extract_rating_value(webpage)
-        if value == "" or claim == "":
+        rating = fatabyyano.extract_rating_value(webpage)
+        if tags == [] or claim == "" or rating == "" or claim == "" or date == "":
             print(line)  # print url
+        else:
+            print(rating + " --> " +
+                  f.FatabyyanoFactCheckingSiteExtractor.translate_rating_value(rating))
+            print(tags)
+
         """
         print(claim)
         print(value)
